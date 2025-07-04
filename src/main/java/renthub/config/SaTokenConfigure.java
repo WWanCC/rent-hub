8980c2c3d15d4d611557da3025e1c702f573e458
Bai5 <bai5_w@163.com>
2025-07-04 12:00:54 +0800

    切换到 sa-token默认模式(不使用jwt.秘钥无法配置，Mixin模式不支持踢人下线api,simple模式没有特别优势)

--- src/main/java/renthub/config/SaTokenConfigure.java
+++ src/main/java/renthub/config/SaTokenConfigure.java
@@ -1,15 +1,17 @@
-package renthub.config;
-
-import cn.dev33.satoken.jwt.StpLogicJwtForMixin;
-import cn.dev33.satoken.stp.StpLogic;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SaTokenConfigure {
-    // Sa-Token 整合 jwt (Mixin 混入模式)
-    @Bean
-    public StpLogic getStpLogicJwt() {
-        return new StpLogicJwtForMixin();
-    }
-}
+//package renthub.config;
+//
+//import cn.dev33.satoken.jwt.StpLogicJwtForMixin;
+//import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
+//import cn.dev33.satoken.stp.StpLogic;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SaTokenConfigure {
+//    // Sa-Token 整合 jwt (Mixin 混入模式)
+//    @Bean
+//    public StpLogic getStpLogicJwt() {
+////        return new StpLogicJwtForMixin();
+//        return new StpLogicJwtForSimple();
+//    }
+//}
