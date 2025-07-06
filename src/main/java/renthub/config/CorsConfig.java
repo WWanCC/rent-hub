11ad2b0df2423c3b136601e118e97aa71f42bece
Bai5 <bai5_w@163.com>
2025-07-06 16:21:54 +0800

    配置跨域 @Value

--- src/main/java/renthub/config/CorsConfig.java
+++ src/main/java/renthub/config/CorsConfig.java
@@ -1,15 +1,20 @@
-package renthub.controller;
+package renthub.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${spring.graphql.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 允许所有路径
-                .allowedOrigins("http://localhost:5173") // 允许前端的源
+                .allowedOrigins(allowedOrigins) // 允许前端的源
                 // .allowedOrigins("*") // 或者使用 "*" 允许所有来源，但生产环境中不推荐
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的HTTP方法
                 .allowedHeaders("*") // 允许所有请求头
