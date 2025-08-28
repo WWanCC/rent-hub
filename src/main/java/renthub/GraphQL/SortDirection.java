183da311bdc3a5043da932e1c738314bb8dc603e
Administrator <bai5_w@163.com>
2025-08-29 00:41:23 +0800

    补充Graphql 排序类型

---
+++ src/main/java/renthub/GraphQL/SortDirection.java
@@ -0,0 +1,17 @@
+package renthub.GraphQL;
+
+/**
+ * API层专用的排序方向枚举。
+ * 与 GraphQL Schema 中的 SortDirection 定义完全对应。
+ */
+public enum SortDirection {
+    /**
+     * 升序
+     */
+    ASC,
+
+    /**
+     * 降序
+     */
+    DESC;
+}
